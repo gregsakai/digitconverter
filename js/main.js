ec86fc0b5cd7395f44a4814322eb923530aacd64
gregsakai <gregsakai@gmail.com>
2017-12-17 11:10:10 -0800

    added index/js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+var input = document.getElementById("mainInput");
+var button = document.getElementById("convertButton");
+
+button.addEventListener("click", convert());
+
+function convert(){
+  
+}
