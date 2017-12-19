72d19390e9e0130c88c976be460dfd956196c355
gregsakai <gregsakai@gmail.com>
2017-12-19 11:56:37 -0800

    some styling

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ var input = document.getElementById("mainInput");
 var button = document.getElementById("convertButton");
 var display = document.getElementById("displayDiv");
 
-button.addEventListener("click", convert());
-
-function convert(){
-  
-}
+button.addEventListener("click", function(){
+  display.innerHTML = input.value;
+});
