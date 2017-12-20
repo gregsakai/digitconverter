43da5f41535406638e85a38b1b8a679370b77e94
gregsakai <gregsakai@gmail.com>
2017-12-19 17:34:16 -0800

    tested array

--- js/main.js
+++ js/main.js
@@ -2,32 +2,56 @@
 var input = document.getElementById("mainInput");
 var button = document.getElementById("convertButton");
 var display = document.getElementById("displayDiv");
-
+/*
+var romanNums = [
+  "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
+];
+var standardNums = [
+  1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
+];
+*/
 var romanNums = {
-  1:"I",
-  4:"IV",
-  5:"V",
-  9:"IX",
-  10:"X",
-  40:"XL",
-  50:"L",
-  90:"XC",
-  100:"C",
-  400:"CD",
-  500:"D",
-  900:"CM",
-  1000:"M"
-};
+  M:1000,
+  CM:900,
+  D:500,
+  CD:400,
+  C:100,
+  XC:90,
+  L:50,
+  XL:40,
+  X:10,
+  IX:9,
+  V:5,
+  IV:4,
+  I:1
+}
 
 button.addEventListener("click", function(){
 
   var charCount = input.value.length;
   console.log(charCount);
+  console.log(num);
+
+  var i = 0
+  var str = "";
+  var num = input.value;
 
   if(input.value<0){
     display.innerHTML = "Please enter a value greater than 0!";
   } else {
-      display.innerHTML = input.value;
+      //display.innerHTML = romanNums.num;
+      // for(i=0; i<charCount; i++){
+      //   while(num >= standardNums[i]){
+      //     display.innerHTML = romanNums[i];
+      //     num = num - standardNums[i];
+      //   }
+      // }
+      for(var i in romanNums){
+        while(num>=romanNums[i]){
+          str += i;
+          num -= romanNums[i];
+        }
+      }
     }
-    
+
 });
