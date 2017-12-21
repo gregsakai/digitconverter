1e65a5f97fd6318bb59ae6c717b8f0c0037fd1e6
gregsakai <gregsakai@gmail.com>
2017-12-21 15:49:49 -0800

    num calls object

--- js/main.js
+++ js/main.js
@@ -2,70 +2,34 @@
 var input = document.getElementById("mainInput");
 var button = document.getElementById("convertButton");
 var display = document.getElementById("displayDiv");
-/*
-var romanNums = [
-  "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
-];
-var standardNums = [
-  1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
-];
-*/
+
 var romanNums = {
-  // M:1000,
-  // CM:900,
-  // D:500,
-  // CD:400,
-  // C:100,
-  // XC:90,
-  // L:50,
-  // XL:40,
-  // X:10,
-  // IX:9,
-  // V:5,
-  // IV:4,
-  // I:1
-  "5":"V",
-  "10":"X"
+  1000:"M",
+  900:"CM",
+  500:"D",
+  400:"CD",
+  100:"C",
+  90:"XC",
+  50:"L",
+  40:"XL",
+  10:"X",
+  9:"IX",
+  5:"V",
+  4:"IV",
+  1:"I"
 }
 
 button.addEventListener("click", function(){
 
-  var romanNums = {
-    // M:1000,
-    // CM:900,
-    // D:500,
-    // CD:400,
-    // C:100,
-    // XC:90,
-    // L:50,
-    // XL:40,
-    // X:10,
-    // IX:9,
-    // V:5,
-    // IV:4,
-    // I:1
-    5:"V",
-    "5":"V",
-    "10":"X",
-    five:"V",
-  }
-
   var charCount = input.value.length;
   var num = input.value;
-  console.log("Number of characters: "+charCount+", Number: "+num);
-
-  var i = 0
-  var str = "";
-
   var parseNum = JSON.parse(num);
-  var strNum = JSON.stringify(num);
-  console.log(parseNum + strNum);
-
+  console.log("Number of digits: "+charCount+", Number: "+parseNum);
 
   if(input.value<0){
     display.innerHTML = "Please enter a value greater than 0!";
   } else {
-      display.innerHTML = romanNums.parseNum;
+      display.innerHTML = romanNums[parseNum];
       // for(i=0; i<charCount; i++){
       //   while(num >= standardNums[i]){
       //     display.innerHTML = romanNums[i];
