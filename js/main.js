85c74afa51e8a0789b18de50845fecb69db694ab
gregsakai <gregsakai@gmail.com>
2017-12-21 15:36:04 -0800

    tried parse/stringify

--- js/main.js
+++ js/main.js
@@ -11,47 +11,73 @@ var standardNums = [
 ];
 */
 var romanNums = {
-  M:1000,
-  CM:900,
-  D:500,
-  CD:400,
-  C:100,
-  XC:90,
-  L:50,
-  XL:40,
-  X:10,
-  IX:9,
-  V:5,
-  IV:4,
-  I:1
+  // M:1000,
+  // CM:900,
+  // D:500,
+  // CD:400,
+  // C:100,
+  // XC:90,
+  // L:50,
+  // XL:40,
+  // X:10,
+  // IX:9,
+  // V:5,
+  // IV:4,
+  // I:1
+  "5":"V",
+  "10":"X"
 }
 
 button.addEventListener("click", function(){
 
+  var romanNums = {
+    // M:1000,
+    // CM:900,
+    // D:500,
+    // CD:400,
+    // C:100,
+    // XC:90,
+    // L:50,
+    // XL:40,
+    // X:10,
+    // IX:9,
+    // V:5,
+    // IV:4,
+    // I:1
+    5:"V",
+    "5":"V",
+    "10":"X",
+    five:"V",
+  }
+
   var charCount = input.value.length;
-  console.log(charCount);
-  console.log(num);
+  var num = input.value;
+  console.log("Number of characters: "+charCount+", Number: "+num);
 
   var i = 0
   var str = "";
-  var num = input.value;
+
+  var parseNum = JSON.parse(num);
+  var strNum = JSON.stringify(num);
+  console.log(parseNum + strNum);
+
 
   if(input.value<0){
     display.innerHTML = "Please enter a value greater than 0!";
   } else {
-      //display.innerHTML = romanNums.num;
+      display.innerHTML = romanNums.parseNum;
       // for(i=0; i<charCount; i++){
       //   while(num >= standardNums[i]){
       //     display.innerHTML = romanNums[i];
       //     num = num - standardNums[i];
       //   }
       // }
-      for(var i in romanNums){
-        while(num>=romanNums[i]){
-          str += i;
-          num -= romanNums[i];
-        }
-      }
+      // for(var i in romanNums){
+      //   while(num>=romanNums[i]){
+      //     str += i;
+      //     num -= romanNums[i];
+      //   }
+      // }
     }
 
 });
