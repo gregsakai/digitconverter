dc20b47f5cf6497b65b2da546dc86e1dc327efac
gregsakai <gregsakai@gmail.com>
2017-12-21 16:01:08 -0800

    adding binary testing files

---
+++ binary/js/main.js
@@ -0,0 +1,16 @@
+
+var input = document.getElementById("mainInput");
+var button = document.getElementById("convertButton");
+var display = document.getElementById("displayDiv");
+
+// BINARY VERSION - MERGE LATER
+
+button.addEventListener("click", function(){
+
+  if(input.value<0){
+    display.innerHTML = "Please enter a value greater than or equal to zero!";
+  } else {
+
+    }
+
+});
