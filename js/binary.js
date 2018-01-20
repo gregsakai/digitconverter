81cae73546b7df33a1a84a9cdd664ff8e0105e94
gregsakai <gregsakai@gmail.com>
2018-01-19 22:03:59 -0800

    condensed media queries

--- js/binary.js
+++ js/binary.js
@@ -1,26 +1,19 @@
-
 var input = document.getElementById("mainInput");
 var button = document.getElementById("convertButton");
 var display = document.getElementById("displayDiv");
 
 display.style.wordWrap = "break-word";
 
-function convertNum(){
+function convertNum() {
   var num = input.value;
   var str = Number(num).toString(2);
-    if(input.value>=0){
-        display.innerHTML = "0"+str;
-      } else {
-        display.innerHTML = "1"+Math.abs(str);
-      }
+  display.innerHTML = str;
 }
 
 button.addEventListener("click", convertNum);
 
-input.addEventListener("keyup", function(ev){
-  if(ev.keyCode == 13){
+input.addEventListener("keyup", function(ev) {
+  if (ev.keyCode == 13) {
     convertNum();
-  } else {
-    console.log("Press enter to execute function");
   }
 });
