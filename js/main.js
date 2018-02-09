b95df1a86ab00202540684b94878592f03c48e82
gregsakai <gregsakai@gmail.com>
2018-02-08 20:14:05 -0800

    added special characters

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
-
 var input = document.getElementById("mainInput");
 var button = document.getElementById("convertButton");
 var display = document.getElementById("displayDiv");
 
 display.style.wordWrap = "break-word";
 
-function convertNum(){
+function convertNum() {
   var num = input.value;
   var str = "";
-  var romanNums = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
-  var standardNums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var romanNums = ["M", "D", "C", "L", "X", "V", "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+  var standardNums = [1000000, 500000, 100000, 50000, 10000, 5000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
-  for(var i=0; i<standardNums.length; i++){
-    while(num >= standardNums[i]){
+  // Adds overline to create special characters for larger numbers
+  for (var spec = 0; spec < 6; spec++)
+    romanNums[spec] = "<span style='text-decoration: overline'>" + romanNums[spec] + "</span>";
+
+  for (var i = 0; i < standardNums.length; i++) {
+    while (num >= standardNums[i]) {
       str += romanNums[i];
       num -= standardNums[i];
     }
@@ -23,8 +26,8 @@ function convertNum(){
 
 button.addEventListener("click", convertNum);
 
-input.addEventListener("keyup", function(ev){
-  if(ev.keyCode == 13){
+input.addEventListener("keyup", function(ev) {
+  if (ev.keyCode == 13) {
     convertNum();
   }
 });
